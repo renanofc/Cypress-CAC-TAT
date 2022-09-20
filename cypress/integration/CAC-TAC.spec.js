eda995e002a2119bf3b33e9c06266ef264013624
Renan <renan@fixpays-MacBook-Pro.local>
2022-09-20 19:47:58 -0300

    Update

--- cypress/integration/CAC-TAC.spec.js
+++ cypress/integration/CAC-TAC.spec.js
@@ -89,4 +89,9 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         cy.get('.error').should('be.visible')
     })
 
+    it('envia o formuário com sucesso usando um comando customizado', function() {
+
+        
+
+    })
 })
