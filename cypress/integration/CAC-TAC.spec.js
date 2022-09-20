311c8d3cfc1be7fd3a632e1408fac83bb9ff8929
Renan <renan@fixpays-MacBook-Pro.local>
2022-09-20 17:17:48 -0300

    Update

--- cypress/integration/CAC-TAC.spec.js
+++ cypress/integration/CAC-TAC.spec.js
@@ -10,12 +10,12 @@ describe('Central de Atendimento ao Cliente TAT', function() {
     })
 
     it('preenche os campos obrigatórios e envia o fórmulario', function() {
-        
+        const longText = 'teste, teste, teste, teste, teste, teste, teste, teste, teste, teste'
         cy.get('#firstName').type('Renan');
         cy.get('#lastName').type('Moreira');
         cy.get('#email').type('renanmoreira.po@gmail.com');
         cy.get('#phone').type('85992169174');
-        cy.get('#open-text-area').type('Estou apenas testando em cypress');
+        cy.get('#open-text-area').type(longText, { delay: 0});
         cy.get('button[type="submit"]').click();
         cy.wait(1000);
         cy.get('.success').should('be.visible'); // o ponto antes do success é para identificar que é uma classe
