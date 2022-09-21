fcda3cbfc868d56a699be054e482d32d8208e3f0
Renan <renan@fixpays-MacBook-Pro.local>
2022-09-21 15:20:14 -0300

    Update

---
+++ cypress/support/gui_commands.js
@@ -0,0 +1,9 @@
+Cypress.Commands.add('renan', function() {
+    cy.get('#firstName').type('Renan')
+    cy.get('#lastName').type('Moreira')
+    cy.get('#email').type('renanmoreira.po@gmail.com')
+    cy.get('#phone').type('85992169174')
+    cy.get('#open-text-area').type('teste')
+    cy.get('button[type="submit"]').click()
+    cy.get('.success').should('be.visible') // o ponto antes do success é para identificar que é uma classe   
+})
