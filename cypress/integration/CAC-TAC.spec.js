67704bd574c2a8a404871deb283cf94befdd1b44
Renan <renan@fixpays-MacBook-Pro.local>
2022-09-22 14:25:55 -0300

    Update

--- cypress/integration/CAC-TAC.spec.js
+++ cypress/integration/CAC-TAC.spec.js
@@ -118,13 +118,23 @@ describe('Central de Atendimento ao Cliente TAT', function() {
 
     })
 
-    it('marca cada tipo de atendimento', function() {
-        cy.get('input[type="radio"]')
-            .should('have.lenght', 3)
-            .each(function($radio) {
-                cy.wrap($radio).check() // cy.wrap - Encapsula como um objeto do cypress
-                cy.wrap($radio).should('be.checked')
-            })
+   // it('marca cada tipo de atendimento', function() {
+   //     cy.get('input[type="radio"]')
+   //         .should('have.lenght', 3)
+   //         .each(function($radio) {
+   //             cy.wrap($radio).check() // cy.wrap - Encapsula como um objeto do cypress
+   //             cy.wrap($radio).should('be.checked')
+   //         })
+
+   // })
+
+   it('marca ambos checkboxes, depois desmarca o último', function() {
+        cy.get('input[type="checkbox"]')
+            .check()
+            .last()
+            .uncheck()
+            .should('not.be.checked')
 
     })
+
 })
